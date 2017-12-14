419f7e6602f40c3cd1b4c21d0e8816379fb507ba
starliiit <dying.flare@gmail.com>
2017-12-14 15:07:08 +0800

    correct nms

--- canny.py
+++ canny.py
@@ -1,50 +1,16 @@
 import cv2
 import numpy as np
-
-# kernel_gx = np.array([
-#     [-1, 0, 1],
-#     [-2, 0, 2],
-#     [-1, 0, 1]
-# ])
-
-# kernel_gy = np.array([
-#     [1, 2, 1],
-#     [0, 0, 0],
-#     [-1, -2, -1]
-# ])
-
-# kernel_gx = np.array([
-#     [0, 1, 1],
-#     [0, -1, -1],
-#     [0, 0, 0]
-# ]) *2
-
-# kernel_gy = np.array([
-#     [0, 1, -1],
-#     [0, 1, -1],
-#     [0, 0, 0]
-# ]) *2
+from math import tan
 
 kernel_gx = np.array([
     [1, 1],
     [-1, -1],
-]) *2
+]) * 2
 
 kernel_gy = np.array([
     [1, -1],
     [1, -1]
-]) *2
-
-nearby_indexes = [
-    [1, 0],
-    [1, 1],
-    [0, 1],
-    [-1, 1],
-    [-1, 0],
-    [-1, -1],
-    [0, -1],
-    [1, -1]
-]
+]) * 2
 
 
 def calc_grad(image):
@@ -52,34 +18,49 @@ def calc_grad(image):
     gx = cv2.filter2D(blur, cv2.CV_64F, kernel_gx)
     gy = cv2.filter2D(blur, cv2.CV_64F, kernel_gy)
     grad = np.sqrt(np.square(gx) + np.square(gy))
-    angles = np.arctan2(gy, gx)
-    return (grad, angles)
+    tgs = gy / gx
+    tgs[np.isnan(tgs)] = 0
+    return (grad, tgs)
 
 
-def map_angles(theta):
-    """
-    arctan2 返回 (-pi, pi]（-pi是取不到的）,将其小于0的部分加上2pi，映射为 [0, 2) 区间
-    乘上4映射为 [0, 8) 区间，将所有元素截断取整（等价于将区间划成八块）
-    最大值抑制时要访问的相邻点的坐标差[m,n]按逆时针顺序保存在列表 nearby_indexes 中
-    """
-    fracs = theta / np.pi  # [-1, 1]
-    neg_fracs = np.minimum(fracs, 0)
-    pos_fracs = np.clip(fracs, 0, 1)
-    neg_fracs[neg_fracs < 0] += 2  # from [-1, 0] to [1, 2]
-    indexes = ((pos_fracs + neg_fracs) * 4).astype(int)  # `*4` means `*180/45`
-    return indexes
-
-
-def nms(grad, angles):
-    indexes = map_angles(angles)
+def nms(grad, tgs):
     after_nms = np.zeros(grad.shape)
     height, width = grad.shape
+    points_nearby = []
     for i in range(height):
         for j in range(width):
-            index_1 = indexes[i][j]
-            index_2 = (index_1 + 1) % 8
-            grad_nearby_1 = get_nearby_grad(grad, i, j, index_1, height, width)
-            grad_nearby_2 = get_nearby_grad(grad, i, j, index_2, height, width)
+            t = tgs[i][j]
+            if t >= tan(-np.pi / 8) and t <= tan(np.pi / 8):
+                points_nearby = [
+                    [i + 1, j],
+                    [i - 1, j]
+                ]
+            elif t > tan(np.pi / 8) and t <= tan(np.pi * 3 / 8):
+                points_nearby = [
+                    [i + 1, j + 1],
+                    [i - 1, j - 1]
+                ]
+            elif t > tan(-np.pi * 3 / 8) and t < tan(-np.pi / 8):
+                points_nearby = [
+                    [i + 1, j - 1],
+                    [i - 1, j + 1]
+                ]
+            elif t > tan(np.pi * 3 / 8) or t < tan(-np.pi * 3 / 8):
+                points_nearby = [
+                    [i, j + 1],
+                    [i, j - 1]
+                ]
+            else:
+                print(t)
+                raise Exception('bad tangent value')
+
+            p1, p2 = points_nearby
+            grad_nearby_1 = 0
+            grad_nearby_2 = 0
+            if validate_index(p1[0], p1[1], height, width):
+                grad_nearby_1 = grad[p1[0]][p1[1]]
+            if validate_index(p2[0], p2[1], height, width):
+                grad_nearby_2 = grad[p2[0]][p2[1]]
             if grad[i][j] > grad_nearby_1 and grad[i][j] > grad_nearby_2:
                 after_nms[i][j] = grad[i][j]
     return after_nms
@@ -127,17 +108,17 @@ def look_around(img, i, j):
 
 def main():
     img = cv2.imread('lena.jpg', cv2.IMREAD_GRAYSCALE)
-    grad, angles = calc_grad(img)
-    after_nms = nms(grad, angles)
+    grad, tgs = calc_grad(img)
+    after_nms = nms(grad, tgs)
     # t1, t2 = double_thresholding(after_nms, 20, 50)
     # res = connect_img_high(t2, t1)
-    # cv2.imwrite('grad.jpg', grad)
-    # cv2.imwrite('nms.jpg', after_nms)
+    cv2.imwrite('grad2.jpg', grad)
+    cv2.imwrite('nms2.jpg', after_nms)
     # cv2.imwrite('res.jpg', res)
-    cv2.imshow('grad', grad)
-    cv2.imshow('nms', after_nms)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('grad', grad)
+    # cv2.imshow('nms', after_nms)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
